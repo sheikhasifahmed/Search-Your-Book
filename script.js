5902cecc4d69f55688d4194a44c9250736ac8b57
sheikasifahmed <sheikhasifahmed100@gmail.com>
2021-09-02 18:22:44 +0600

    background

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ btnSearch.addEventListener("click", function () {
  ***************************************************/
 
 const showData = (data) => {
+  console.log(data);
   const resultFound = data.numFound;
   const books = data.docs;
   console.log(data, books);
@@ -43,7 +44,7 @@ const showData = (data) => {
   // error handling for no result found
 
   if (resultFound !== 0)
-    totalSearch.innerText = `Showing ${books.length} results of total ${resultFound} for "${inputSearch.value}"...`;
+    totalSearch.innerText = `Showing ${books.length} results of total ${resultFound} for "${inputSearch.value}"`;
   else {
     errorMsg.style.display = "block";
     errorMsg.innerText = `Sorry! No book Found for "${inputSearch.value}"`;
